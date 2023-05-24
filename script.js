c75191773f116a985c4cf1164785b4db07bea1ae
SystemDiadsSAS <133913191+SystemDiadsSAS@users.noreply.github.com>
2023-05-24 15:39:36 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var boton = document.getElementById('contactoboton');
+  boton.addEventListener('click', function() {
+    window.location.href = 'contactopage.html';
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var boton = document.getElementById('nosotrosboton');
+  boton.addEventListener('click', function() {
+    window.location.href = 'nosotrospage.html';
+  });
+});
